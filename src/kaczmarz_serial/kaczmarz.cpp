fb92ddd9aa547e9cb071a0ffe10898304de34a59
Viktor Fukala <{ID}+{username}@users.noreply.github.com>
2024-10-19 17:17:26 +0200

    Hopefully improve Kaczmarz implementation readability

--- src/kaczmarz_serial/kaczmarz.cpp
+++ src/kaczmarz_serial/kaczmarz.cpp
@@ -4,26 +4,28 @@
 
 KaczmarzSolverStatus kaczmarz_solver(const double *A, const double *b, double *x, unsigned rows, unsigned cols, unsigned max_iterations, double precision) {
   for (unsigned iter = 0; iter < max_iterations; iter++) {
-    unsigned converged = 1;
+    // the algorithm has converged iff none of the rows in an iteration caused a substantial correction
+    bool substantial_correction = false;
     for (unsigned i = 0; i < rows; i++) {
       double dot_product = 0.0;
-      double a_norm = 0.0;
+      double row_sq_norm = 0.0;
+      const double *const a_row = A + i * cols;
       for (unsigned j = 0; j < cols; j++) {
-        dot_product += A[i*cols + j] * x[j];
-        a_norm += A[i*cols + j] * A[i*cols + j];
+        dot_product += a_row[j] * x[j];
+        row_sq_norm += a_row[j] * a_row[j];
       }
-      if (a_norm < 1e-10) {
+      if (row_sq_norm < 1e-10) {
         return KaczmarzSolverStatus::ZeroNormRow;
       }
-      double correction = (b[i] - dot_product) / (a_norm);
+      const double correction = (b[i] - dot_product) / row_sq_norm;
       for (unsigned j = 0; j < cols; j++) {
         x[j] += A[i*cols + j] * correction;
       }
       if (std::fabs(correction) > precision) {
-        converged = 0; //signal, that algorithm hasnt converged yet
+        substantial_correction = true;
       }
     }
-    if (converged) {
+    if (!substantial_correction) {
       return KaczmarzSolverStatus::Converged;
     }
   }
