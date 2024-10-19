0e9041cdfa3f543c03fe65b8e586c23720d892c1
Viktor Fukala <{ID}+{username}@users.noreply.github.com>
2024-10-19 16:34:48 +0200

    Use array indexing syntax in Kaczmarz implementation

--- src/kaczmarz_serial/kaczmarz.c
+++ src/kaczmarz_serial/kaczmarz.c
@@ -10,16 +10,16 @@ void kaczmarz_solver(double *A, double *b, double *x, unsigned rows, unsigned co
       double dot_product = 0.0;
       double a_norm = 0.0;
       for (unsigned j = 0; j < cols; j++) {
-        dot_product += *(A + i*cols + j) * *(x + j);
-        a_norm += *(A + i*cols + j) * *(A + i*cols + j);
+        dot_product += A[i*cols + j] * x[j];
+        a_norm += A[i*cols + j] * A[i*cols + j];
       }
       if (a_norm < 1e-10) {
         printf("Matrix row with 0 norm, iteration not possible.\n");
         return;
       }
-      double correction = (*(b+i) - dot_product) / (a_norm);
+      double correction = (b[i] - dot_product) / (a_norm);
       for (unsigned j = 0; j < cols; j++) {
-        *(x+j) += *(A +i*cols + j) * correction;
+        x[j] += A[i*cols + j] * correction;
       }
       if (fabs(correction) > precision) {
         converged = 0; //signal, that algorithm hasnt converged yet
