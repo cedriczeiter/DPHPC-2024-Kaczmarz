fbcbfe61a1e5362483114a77422efa1d542b0481
Clemens Herfarth <cherfarth@ethz.ch>
2024-10-25 20:26:28 +0200

    cleanup of restructure

--- src/kaczmarz_serial/kaczmarz_common.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef KACZMARZ_COMMON_HPP
-#define KACZMARZ_COMMON_HPP
-
-/**
- * @brief Enumeration to describe the result of the Kaczmarz solver.
- */
-enum class KaczmarzSolverStatus { Converged, ZeroNormRow, OutOfIterations };
-
-#endif  // KACZMARZ_COMMON_HPP
