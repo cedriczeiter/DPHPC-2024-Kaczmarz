a2dc9ce255c317b421c40f87863d37a609cf0fed
Clemens Herfarth <cherfarth@ethz.ch>
2024-10-25 19:52:48 +0200

    added reading in config data from json file

--- mesh_builder/src/main.cpp
+++ mesh_builder/src/main.cpp
@@ -1,29 +1,71 @@
+#include <fstream>
+#include <lf/assemble/assemble.h>
+#include <lf/fe/fe.h>
 #include <lf/io/io.h>
 #include <lf/mesh/hybrid2d/hybrid2d.h>
 #include <lf/mesh/mesh_factory.h>
+#include <lf/mesh/test_utils/test_meshes.h>
 #include <lf/mesh/utils/utils.h>
 
 #include <filesystem>
 #include <iostream>
+#include <nlohmann/json.hpp>
 
-int main() {
-  std::cout << "LehrFEM++ DEMO: mesh capabilities and functionality" << '\n';
-  // Set complete file path to the sample mesh in Gmsh format
+int main(int argc, char **argv) {
+  // Set complete file path to the configuration file;
   const std::filesystem::path here = __FILE__;
-  auto mesh_file = here.parent_path() / "../meshes/unit_square.msh";
+  auto config_file_path = here.parent_path() / "../configuration.json";
 
-  // Create a 2D mesh data structure from the information contained in
-  // the file A factory object is in charge of
-  // creating mesh entities and has to be initialized first.
-  auto factory = std::make_unique<lf::mesh::hybrid2d::MeshFactory>(2);
-  lf::io::GmshReader reader(std::move(factory), mesh_file.string());
-  // Obtain pointer to read only mesh from the mesh reader object
-  const std::shared_ptr<const lf::mesh::Mesh> mesh_ptr = reader.mesh();
-  const lf::mesh::Mesh &mesh{*mesh_ptr};
+  // read in data from configuration file; right now we dont have many params in
+  // there yet, but this mightchange, so having everything in a config file will
+  // be much easier
+  std::ifstream config_file(config_file_path.string());
+  if (!config_file.is_open()) {
+    std::cerr << "Error opening config file!" << std::endl;
+    return 1;
+  }
+  nlohmann::json config_data;
+  config_file >> config_data;
+  config_file.close();
 
-  // Output general information on mesh; self-explanatory
-  std::cout << "Mesh from file " << mesh_file.string() << ": ["
-            << mesh.DimMesh() << ',' << mesh.DimWorld() << "] dim:" << '\n';
-  std::cout << mesh.NumEntities(0) << " cells, " << mesh.NumEntities(1)
-            << " edges, " << mesh.NumEntities(2) << " nodes" << '\n';
+  unsigned selector_mesh = config_data["selector"];
+  double scale = config_data["scale"];
+  // generate mesh
+  auto mesh_p =
+      lf::mesh::test_utils::GenerateHybrid2DTestMesh(selector_mesh, scale);
+  // Create HierarchicalFESpace with uniform degree 5.
+  const unsigned degree = 5;
+  const auto fe_space =
+      std::make_shared<lf::fe::HierarchicScalarFESpace<double>>(mesh_p, degree);
+
+  // define diffusion coefficient
+  const lf::mesh::utils::MeshFunctionConstant mf_alpha(1);
+  // define rhs load
+  auto mf_load = lf::mesh::utils::MeshFunctionConstant(1.);
+
+  // Assemble the system matrix and right hand side
+  Eigen::VectorXd rhs = Eigen::VectorXd::Zero(fe_space->LocGlobMap().NumDofs());
+  const lf::assemble::DofHandler &dofh = fe_space->LocGlobMap();
+  lf::assemble::COOMatrix<double> A_COO(dofh.NumDofs(), dofh.NumDofs());
+
+  lf::fe::DiffusionElementMatrixProvider element_matrix_provider(fe_space,
+                                                                 mf_alpha);
+  AssembleMatrixLocally(0, dofh, dofh, element_matrix_provider, A_COO);
+  lf::fe::ScalarLoadElementVectorProvider element_vector_provider(fe_space,
+                                                                  mf_load);
+  AssembleVectorLocally(0, dofh, element_vector_provider, rhs);
+
+  // Enforce zero dirichlet boundary conditions
+
+  const auto boundary = lf::mesh::utils::flagEntitiesOnBoundary(mesh_p);
+  const auto selector = [&](unsigned int idx) -> std::pair<bool, double> {
+    const auto &entity = dofh.Entity(idx);
+    return {entity.Codim() > 0 && boundary(entity), 0};
+  };
+  FixFlaggedSolutionComponents(selector, A_COO, rhs);
+
+  // Solve the LSE using the cholesky decomposition
+  Eigen::SparseMatrix<double> A = A_COO.makeSparse();
+
+  std::cout << " Hihihiuhuhu " << std::endl;
 }
