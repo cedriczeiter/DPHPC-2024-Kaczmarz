c25b4d073e40a34bbc7e9c6101f38e4667f5ff12
czeiter <czeiter@ethz.ch>
2024-10-28 16:56:06 +0100

    added random method

--- kaczmarz/src/benchmarking.cpp
+++ kaczmarz/src/benchmarking.cpp
@@ -7,12 +7,14 @@
 
 #include "kaczmarz.hpp"
 #include "kaczmarz_common.hpp"
+#include "random_kaczmarz.hpp"
 #include "linear_systems/dense.hpp"
 #include "linear_systems/sparse.hpp"
 
 #define MAX_IT 1000000
-#define BANDWIDTH 5
+#define BANDWIDTH 4
 #define MAX_DIM 16
+#define PRECISION 1e-10
 
 /// @brief Computes the average and standard deviation of a vector of times.
 /// @param times A vector of times recorded for benchmarking.
@@ -62,7 +64,7 @@ double benchmark_normalsolver_dense(const int dim, const int numIterations,
     const auto start = std::chrono::high_resolution_clock::now();
 
     dense_kaczmarz(lse, &x_kaczmarz[0], MAX_IT * dim,
-                   1e-10);  // solve randomised system, max iterations steps
+                   PRECISION);  // solve randomised system, max iterations steps
                             // selected arbitratly, we might need to revise this
 
     auto end = std::chrono::high_resolution_clock::now();
@@ -89,7 +91,32 @@ double benchmark_sarsesolver_sparse(const int dim, const int numIterations,
 
     sparse_kaczmarz(
         lse, x_kaczmarz_sparse, MAX_IT * dim,
-        1e-10);  // solve randomised system, max iterations steps
+        PRECISION);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    times.push_back(elapsed.count());
+  }
+
+  double avgTime = 0;
+  compute_statistics(times, avgTime, stdDev);
+  return avgTime;
+}
+
+double benchmark_randomsolver_dense(const int dim, const int numIterations,
+                                    double& stdDev, std::mt19937& rng) {
+  std::vector<double> times;
+  for (int i = 0; i < numIterations; ++i) {
+    const DenseLinearSystem lse =
+        DenseLinearSystem::generate_random_regular(rng, dim);
+
+  std::vector<double> x_kaczmarz_random(dim, 0.0);
+
+    const auto start = std::chrono::high_resolution_clock::now();
+
+    kaczmarz_random_solver(
+        lse,  &x_kaczmarz_random[0], MAX_IT * dim,
+        PRECISION);  // solve randomised system, max iterations steps
                  // selected arbitratly, we might need to revise this
 
     auto end = std::chrono::high_resolution_clock::now();
@@ -107,7 +134,7 @@ int main() {
   std::mt19937 rng(21);
 
   //////////////////////////////////////////
-  /// Normal Sovler Dense///
+  /// Normal Solver Dense///
   //////////////////////////////////////////
 
   // Open the file for output
@@ -126,12 +153,12 @@ int main() {
   outFileND.close();  // Close the file after writing
 
   //////////////////////////////////////////
-  /// Normal Sovler Dense///
+  /// Normal Solver Sparse///
   //////////////////////////////////////////
 
   // Open the file for output
-  std::ofstream outFileSS("results_sparsesolver_sparse.csv");
-  outFileSS << "Dim,AvgTime,StdDev\n";  // Write the header for the CSV file
+  std::ofstream outFileNS("results_sparsesolver_sparse.csv");
+  outFileNS << "Dim,AvgTime,StdDev\n";  // Write the header for the CSV file
 
   // Loop over problem sizes, benchmark, and write to file
   for (int dim = 1; dim <= MAX_DIM; dim *= 2) {
@@ -140,8 +167,28 @@ int main() {
         benchmark_sarsesolver_sparse(dim, numIterations, stdDev, rng);
 
     // Write results to the file
-    outFileSS << dim << "," << avgTime << "," << stdDev << "\n";
+    outFileNS << dim << "," << avgTime << "," << stdDev << "\n";
   }
-  outFileSS.close();  // Close the file after writing
+  outFileNS.close();  // Close the file after writing
+
+
+    //////////////////////////////////////////
+  /// Random Solver Dense///
+  //////////////////////////////////////////
+
+  // Open the file for output
+  std::ofstream outFileRD("results_randomsolver_dense.csv");
+  outFileRD << "Dim,AvgTime,StdDev\n";  // Write the header for the CSV file
+
+  // Loop over problem sizes, benchmark, and write to file
+  for (int dim = 1; dim <= MAX_DIM; dim *= 2) {
+    double stdDev;
+    double avgTime =
+        benchmark_randomsolver_dense(dim, numIterations, stdDev, rng);
+
+    // Write results to the file
+    outFileRD << dim << "," << avgTime << "," << stdDev << "\n";
+  }
+  outFileRD.close();  // Close the file after writing
   return 0;
 }
