5b55baf7826732f0c3ef00278661e46206f48b6d
zenhug <zenhug@student.ethz.ch>
2024-10-28 18:52:01 +0100

    modify python script to plot all data (#50)
    * Added plotting of all Algorithms to one graph
    
    * Didn't change names of data, now should be correct

--- kaczmarz/src/plot.py
+++ kaczmarz/src/plot.py
@@ -2,15 +2,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-data = pd.read_csv("results.csv")
+data_normal_dense = pd.read_csv("results_normalsolver_dense.csv")
+data_sparse_sparse = pd.read_csv("results_sparsesolver_sparse.csv")
+data_random_dense = pd.read_csv("results_randomsolver_dense.csv")
 
 # Create the plot with error bars and connected lines
-plt.errorbar(data["Dim"], data["AvgTime"], yerr=data["StdDev"], fmt='o-', capsize=5, label="Execution Time")
+plt.errorbar(data_normal_dense["Dim"], data_normal_dense["AvgTime"], yerr=data_normal_dense["StdDev"], fmt='o-', capsize=5, label="Normal Solver Dense")
+
+# Create the plot with error bars and connected lines
+plt.errorbar(data_sparse_sparse["Dim"], data_sparse_sparse["AvgTime"], yerr=data_sparse_sparse["StdDev"], fmt='o-', capsize=5, label="Sparse Solver Sparse")
+
+# Create the plot with error bars and connected lines
+plt.errorbar(data_random_dense["Dim"], data_random_dense["AvgTime"], yerr=data_random_dense["StdDev"], fmt='o-', capsize=5, label="Random Solver Dense")
+
 
 # Set labels and title
 plt.xlabel("Dim")
 plt.ylabel("Average Time (s)")
-plt.title("Benchmark Performance")
+plt.title("Benchmark of our Algorithms")
 
 # Add grid and legend
 plt.grid(True)
