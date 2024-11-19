ec73af47e6444ae6a6b8f15496104a7ce58e7826
czeiter <czeiter@ethz.ch>
2024-11-19 21:00:21 +0100

    comment carp.cpp

--- kaczmarz/src/solvers/carp.cpp
+++ kaczmarz/src/solvers/carp.cpp
@@ -16,32 +16,39 @@ KaczmarzSolverStatus carp_gpu(const SparseLinearSystem& lse, Vector& x,
                               const unsigned max_iterations,
                               const double precision,
                               const unsigned num_threads) {
-  // squared norms of rows of A (so that we don't need to recompute them in each
-  // iteration
 
-  const int* A_outer = lse.A().outerIndexPtr();
-  const int* A_inner = lse.A().innerIndexPtr();
-  const double* A_values = lse.A().valuePtr();
+  // get the sparse matrix in CSR format
+  const int* A_outer = lse.A().outerIndexPtr(); // outer index of the sparse matrix in CSR format (row pointer)
+  const int* A_inner = lse.A().innerIndexPtr(); // inner index of the sparse matrix in CSR format (column indices)
+  const double* A_values = lse.A().valuePtr(); // non-zero values of the sparse matrix in CSR format
 
+  // get the right-hand side vector
   const double* b = lse.b().data();
+
+  // get information about sparse matrix
   const unsigned rows = lse.row_count();
   const unsigned cols = lse.column_count();
   const unsigned nnz = lse.A().nonZeros();
 
+  // get squared norms of the rows of the matrix (precompute for performance)
+
   std::vector<double> h_sq_norms(rows);
   for (unsigned i = 0; i < rows; i++) {
-    h_sq_norms[i] = lse.A().row(i).dot(lse.A().row(i));
-    if (h_sq_norms[i] < 1e-7) return KaczmarzSolverStatus::ZeroNormRow;
+    // get the row i of the matrix
+    Eigen::SparseVector<double> A_row_i = lse.A().innerVector(i);
+    h_sq_norms[i] = A_row_i.dot(A_row_i);
+
+    if (h_sq_norms[i] < 1e-7) {
+      return KaczmarzSolverStatus::ZeroNormRow; // check for zero norm rows
+    }
   }
 
   // get maximum nr of nnz in row
   int max_nnz_in_row = 0;
   for (unsigned i = 0; i < rows; i++) {
-    // std::cout << "index: " << i << std::endl;
     const int nnz_in_row = A_outer[i + 1] - A_outer[i];
-    if (nnz_in_row > max_nnz_in_row) max_nnz_in_row = nnz_in_row;
+    max_nnz_in_row = std::max(max_nnz_in_row, nnz_in_row);
   }
-  // std::cout << "MAX NNZ: " << max_nnz_in_row << std::endl;
 
   // call carp solver for beginning
   return invoke_carp_solver_gpu(A_outer, A_inner, A_values, b, x.data(),
