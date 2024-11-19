f74f1f9161ebf252cc4163166974d279d7527ba9
czeiter <czeiter@ethz.ch>
2024-11-19 20:47:19 +0100

    doxygen header file comments

--- kaczmarz/src/solvers/asynchronous.hpp
+++ kaczmarz/src/solvers/asynchronous.hpp
@@ -1,18 +1,40 @@
 #ifndef ASYNC_HPP
 #define ASYNC_HPP
+
 #include <unistd.h>
 
 #include "common.hpp"
 #include "linear_systems/sparse.hpp"
 #include "linear_systems/types.hpp"
 
+/**
+ * @brief Solves a sparse linear system using the asynchronous Kaczmarz method on a GPU.
+ *
+ * @param lse The sparse linear system to be solved.
+ * @param x The initial guess for the solution vector, which will be updated with the solution.
+ * @param max_iterations The maximum number of iterations to perform.
+ * @param precision The desired precision for the solution.
+ * @param num_threads The number of threads to use for the computation.
+ * @return The status of the solver after completion.
+ */
 KaczmarzSolverStatus asynchronous_gpu(const SparseLinearSystem &lse, Vector &x,
                                       const unsigned max_iterations,
                                       const double precision,
                                       const unsigned num_threads);
 
+/**
+ * @brief Solves a sparse linear system using the asynchronous Kaczmarz method on a CPU.
+ *
+ * @param lse The sparse linear system to be solved.
+ * @param x The initial guess for the solution vector, which will be updated with the solution.
+ * @param max_iterations The maximum number of iterations to perform.
+ * @param precision The desired precision for the solution.
+ * @param num_threads The number of threads to use for the computation.
+ * @return The status of the solver after completion.
+ */
 KaczmarzSolverStatus asynchronous_cpu(const SparseLinearSystem &lse, Vector &x,
                                       const unsigned max_iterations,
                                       const double precision,
                                       const unsigned num_threads);
-#endif
+
+#endif // ASYNC_HPP
