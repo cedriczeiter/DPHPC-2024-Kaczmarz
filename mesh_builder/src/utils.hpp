3cc44fdd0e903b2d351188ce56ae86c9ae570a63
Clemens Herfarth <cherfarth@ethz.ch>
2024-12-02 11:36:14 +0100

    formatting

--- mesh_builder/src/utils.hpp
+++ mesh_builder/src/utils.hpp
@@ -22,7 +22,7 @@ SparseLinearSystem generate_system(nlohmann::json config_data) {
   auto f1 = [](Eigen::Vector2d x) {
     return 2 * (x[0] - x[0] * x[0]) + 2 * (x[1] - x[1] * x[1]) + 1000;
   };
-  auto alpha1 = [](Eigen::Vector2d) {return 1.; };
+  auto alpha1 = [](Eigen::Vector2d) { return 1.; };
   auto gamma1 = [](Eigen::Vector2d) { return 1; };
 
   // problem 2: -lapl(u) + (x*x + y*y)u = F
@@ -32,25 +32,23 @@ SparseLinearSystem generate_system(nlohmann::json config_data) {
     return 2 * (x - x * x) + 2 * (y - y * y) +
            (x * x + y * y) * (x - x * x) * (y - y * y);
   };
-  auto alpha2 = [](Eigen::Vector2d) {return 1.; };
+  auto alpha2 = [](Eigen::Vector2d) { return 1.; };
   auto gamma2 = [](Eigen::Vector2d x_vec) {
     const double x = x_vec[0];
     const double y = x_vec[1];
     return (x * x + y * y);
   };
 
-  //problem 3: -div(x*y*grad(u)) + x*y*u = F
-  auto f3 = [](Eigen::Vector2d x_vec){
+  // problem 3: -div(x*y*grad(u)) + x*y*u = F
+  auto f3 = [](Eigen::Vector2d x_vec) {
     const double x = x_vec[0];
     const double y = x_vec[1];
-    return -((1-4*x)*(y*y - y*y*y) + (1-4*y)*(x*x - x*x*x)) + (x*x - x*x*x)*(y*y - y*y*y);
+    return -((1 - 4 * x) * (y * y - y * y * y) +
+             (1 - 4 * y) * (x * x - x * x * x)) +
+           (x * x - x * x * x) * (y * y - y * y * y);
   };
-  auto alpha3 = [](Eigen::Vector2d x){
-    return x[0]*x[1];
-  };
-  auto gamma3 = [](Eigen::Vector2d x){
-    return x[0]*x[1];
-  };
+  auto alpha3 = [](Eigen::Vector2d x) { return x[0] * x[1]; };
+  auto gamma3 = [](Eigen::Vector2d x) { return x[0] * x[1]; };
 
   std::vector<std::function<double(Eigen::Vector2d)>> rhs_functions;
   rhs_functions.push_back(f1);
@@ -86,9 +84,7 @@ SparseLinearSystem generate_system(nlohmann::json config_data) {
   const auto fe_space =
       std::make_shared<lf::uscalfe::FeSpaceLagrangeO1<double>>(mesh_p);
 
-
   unsigned problem = config_data["problem"];
-
 
   // define diffusion coefficient
   lf::mesh::utils::MeshFunctionGlobal mf_alpha{alpha_functions.at(problem - 1)};
