18dcce7a6431994f564dc767691291ce54630ac8
Clemens Herdfarth <clemens.herfarth@gmail.com>
2024-12-10 21:54:42 +0100

    wip

--- kaczmarz/src/benchmark_one_carp.cpp
+++ kaczmarz/src/benchmark_one_carp.cpp
@@ -37,7 +37,7 @@ int main() {
 
   // Read in the system from file
   std::ifstream lse_input_stream(
-      "../../generated_bvp_matrices/problem1_complexity5_degree1.txt");
+      "../../generated_bvp_matrices/problem1_complexity6_degree1.txt");
   const SparseLinearSystem sparse_lse =
       SparseLinearSystem::read_from_stream(lse_input_stream);
 
