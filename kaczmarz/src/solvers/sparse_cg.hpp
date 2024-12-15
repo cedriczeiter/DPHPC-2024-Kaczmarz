2a7838f6283bb3d027ba776fc84e7cd48143d1a3
Clemens Herfarth <cherfarth@ethz.ch>
2024-12-15 16:34:27 +0100

    formatting

--- kaczmarz/src/solvers/sparse_cg.hpp
+++ kaczmarz/src/solvers/sparse_cg.hpp
@@ -1,17 +1,14 @@
 #include <chrono>
-#include <cmath>
-#include <iostream>
-#include "common.hpp"
-#include "linear_systems/sparse.hpp"
-
-#include <chrono>
 #include <climits>
+#include <cmath>
 #include <fstream>
 #include <iostream>
 #include <random>
+
+#include "common.hpp"
+#include "linear_systems/sparse.hpp"
 #include "linear_systems/types.hpp"
 
-#include <chrono>
-#include <climits>
-
-KaczmarzSolverStatus sparse_cg(const SparseLinearSystem& lse, Vector& x, const double precision, const unsigned max_iterations);
+KaczmarzSolverStatus sparse_cg(const SparseLinearSystem& lse, Vector& x,
+                               const double precision,
+                               const unsigned max_iterations);
