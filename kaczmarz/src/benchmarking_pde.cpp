70ea9e9bcd37ae679de7ff663c1c949911dace92
zenhug <zenhug@student.ethz.ch>
2024-12-16 19:25:34 +0100

    param

--- kaczmarz/src/benchmarking_pde.cpp
+++ kaczmarz/src/benchmarking_pde.cpp
@@ -920,7 +920,7 @@ int main() {
   make_file_eigen_iterative_better(1, MAX_PROBLEMS, 1, 6, 1, 1, NUM_IT);
   make_file_normal_solver(1, MAX_PROBLEMS, 1, 4, 1, 1, NUM_IT);
   make_file_sparse_cg(1, MAX_PROBLEMS, 1, 6, 1, 1, NUM_IT);
-  make_file_cuda_direct(1, MAX_PROBLEMS, 1, 6, 1, 1, NUM_IT);
+  make_file_cuda_direct(1, MAX_PROBLEMS, 1, 5, 1, 1, NUM_IT);
   make_file_cpu_banded(1, MAX_PROBLEMS, 1, 6, 1, 1, NUM_IT);
   make_file_cuda_banded(1, MAX_PROBLEMS, 1, 6, 1, 1, NUM_IT);
 
