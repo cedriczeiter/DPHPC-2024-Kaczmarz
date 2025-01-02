a760e3bc24593e8ea2fa079a4d3c3d610bf6ff94
Clemens Herfarth <cherfarth@ethz.ch>
2025-01-02 20:33:29 +0100

    change to mean

--- benchmarking_data/compare_running_times/running_times.py
+++ benchmarking_data/compare_running_times/running_times.py
@@ -30,9 +30,21 @@ for path in file_paths:
         df_filtered = df[df['Problem'] == problem]
 
         # Group by dimension and calculate mean and variance of time
-        df_grouped = df_filtered.groupby('Dim').agg({'Time': ['mean', 'var']}).reset_index()
+        df_grouped = df_filtered.groupby('Dim').agg({'Time': ['median', 'var']}).reset_index()
         df_grouped.columns = ['Dim', 'Time_mean', 'Time_var']
         df_grouped['Method'] = method
+        
+        #some renaming:
+        if (method == "architecture"):
+            df_grouped['Method'] = "GPU Carp-CG"
+        elif (method == "cg"):
+            df_grouped['Method'] = "Eigen LeastSquaresCG"
+        elif (method == "direct"):
+            df_grouped['Method'] = "Eigen SparseLU"
+        elif (method == "kaczmarz"):
+            df_grouped["Method"] = "Kaczmarz"
+        elif (method == "cusolver"):
+            df_grouped["Method"] = "NVIDIA cuDSS"
 
         # Store the data in the dictionary
         if problem not in problem_data:
@@ -43,14 +55,15 @@ for path in file_paths:
 # Plotting
 for problem, data in problem_data.items():
     plt.figure(figsize=(12, 6))
+
     sns.scatterplot(x='Dim', y='Time_mean', hue='Method', data=data)
 
     # Add trendlines
     for method in data['Method'].unique():
         method_data = data[data['Method'] == method]
-        z = np.polyfit(np.log(method_data['Dim']), np.log(method_data['Time_mean']), 3)
+        '''z = np.polyfit(np.log(method_data['Dim']), np.log(method_data['Time_mean']), 1)
         p = np.poly1d(z)
-        plt.plot(method_data['Dim'], np.exp(p(np.log(method_data['Dim']))), linestyle='--', label=f'{method} Trendline')
+        plt.plot(method_data['Dim'], np.exp(p(np.log(method_data['Dim']))), linestyle='--', label=f'{method} Trendline')'''
 
     # Customize the plot
     plt.xscale('log')
