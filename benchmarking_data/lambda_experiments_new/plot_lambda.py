4e7dd498deda3bcbefd0f98ff036a2a757bc4816
Clemens Herfarth <cherfarth@ethz.ch>
2025-01-07 11:24:46 +0100

    normalizing lambda experiments

--- benchmarking_data/lambda_experiments_new/plot_lambda.py
+++ benchmarking_data/lambda_experiments_new/plot_lambda.py
@@ -46,6 +46,15 @@ complexity_markers = {
     '6': 'p'
 }
 
+dimensions = {
+    '1': 30,
+    '2': 103,
+    '3': 381,
+    '4': 1465,
+    '5': 5745,
+    '6': 22753
+}
+
 # Create a colormap
 colormap = cm.get_cmap('tab10')
 degree_colors = {}
@@ -55,33 +64,35 @@ plt.figure(figsize=(10, 6))
 for key, df in data.items():
     degree = key.split(', ')[2][-1]
     complexity = key.split(', ')[1][-1]
-    if degree not in degree_colors:
-        degree_colors[degree] = colormap(len(degree_colors) / 10.0)
-    plt.plot(df['Relaxation'], df['Carp_steps'], marker=complexity_markers[complexity], linestyle='--', label=key, color=degree_colors[degree], linewidth=1)  # Set linewidth to 1
+    if not (complexity == '1' or complexity == '2' or complexity == '3'):
+        if degree not in degree_colors:
+            degree_colors[degree] = colormap(len(degree_colors) / 10.0)
+        plt.plot(df['Relaxation'], df['Carp_steps']/df['Carp_steps'].min(), marker=complexity_markers[complexity], linestyle='--', label=key, color=degree_colors[degree], linewidth=1)  # Set linewidth to 1
 
      # Highlight the minima
-    if not df.empty:
+    '''if not df.empty:
         min_index = df['Carp_steps'].idxmin()
     else:
         print(f"No valid data points in {file}")
         continue
     min_relaxation = df.loc[min_index, 'Relaxation']
     min_carp_steps = df.loc[min_index, 'Carp_steps']
-    plt.plot(min_relaxation, min_carp_steps,  marker=complexity_markers[complexity],  color='red', markersize=10, label=f'Min {key}')
+    plt.plot(min_relaxation, min_carp_steps,  marker=complexity_markers[complexity],  color='red', markersize=10, label=f'Min {key}')'''
 
 
 plt.xlabel('Relaxation-Parameter')
 plt.ylabel('Iterations')
 plt.xlim(0.1352, 0.71)
+plt.ylim(0.9, 2)
 plt.title('Iterations vs Relaxation-Parameter')
 plt.grid(True)
 
-x_coord = 0.18
+'''x_coord = 0.18
 x_coord_number = 0.145
 interval_hor_size = 0.005
 # Draw an interval for problem 1
 interval_x = [x_coord, x_coord]
-interval_y = [0, 2000]
+interval_y = [1, 2000]
 plt.plot(interval_x, interval_y, color='magenta', lw=2)
 plt.plot([interval_x[0] - interval_hor_size, interval_x[0] + interval_hor_size], [interval_y[0], interval_y[0]], color='magenta', lw=2)
 plt.plot([interval_x[1] - interval_hor_size, interval_x[1] + interval_hor_size], [interval_y[1], interval_y[1]], color='magenta', lw=2)
@@ -106,7 +117,7 @@ plt.plot([interval_x[1] - interval_hor_size, interval_x[1] + interval_hor_size],
 # Add a big magenta three to the plot
 plt.text(x_coord_number, 19500, 'P3', fontsize=20, color='magenta')
 
-
+'''
 
 # Create custom legend handles
 marker_handles = [Line2D([0], [0], marker=marker, color='w', markerfacecolor='k', markersize=10, linestyle='None') for marker in complexity_markers.values()]
@@ -115,10 +126,10 @@ minima_handle = [Line2D([0], [0], marker='o', color='w', markerfacecolor='r', ma
 
 # Combine marker and color handles
 handles = marker_handles + color_handles + minima_handle
-labels = [f'Complexity {k}' for k in complexity_markers.keys()] + [f'Degree {k}' for k in degree_colors.keys()] + ['Minima']
+labels = [f'Degree n = {dimensions.get(k)}' for k in complexity_markers.keys()] + [f'Degree {k}' for k in degree_colors.keys()] + ['Minima']
 
 # Create combined legend
-plt.legend(handles, labels, loc='upper right', title='', framealpha=1.0)
+#plt.legend(handles, labels, loc='upper right', title='', framealpha=1.0)
 
 plt.savefig('lambda_experiments.eps', format='eps', bbox_inches='tight')
 plt.savefig('lambda_experiments.png', format='png', bbox_inches='tight')
